85bd912af81315f2361d708191dd5c04d47a16e8
Jeremy <jeremygm1@hotmail.com>
2020-02-20 14:45:39 -0500

    Commit

--- Contrato/Contrato.aspx.cs
+++ Contrato/Contrato.aspx.cs
@@ -1,29 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.IO;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
-using IronPdf;
 
 namespace Contrato
 {
   public partial class Contrato : System.Web.UI.Page
-  {   
+  {    
     protected void Page_Load(object sender, EventArgs e)
     {
       SqlConnection conn = new SqlConnection(@"Data Source=190.145.65.244;Initial Catalog=CTD_DMS;Persist Security Info=True;User ID=UsoExclusivoCTD;Password=C7d5452014");
-      SqlCommand query = new SqlCommand(@"EXECUTE spn_contrato 98531705, 62100", conn);
+      SqlCommand buscarContrato = new SqlCommand(@"EXECUTE spn_contrato 98531705, 62100", conn);
       conn.Open();      
 
-      SqlDataReader dr = query.ExecuteReader();
+      SqlDataReader dr = buscarContrato.ExecuteReader();
       if(dr.Read())
       {
         TextBox1.Text = (dr["descripcion_contrato"].ToString());
       }
       conn.Close();
+    }
+
+    internal void CreatePdf(string dEST)
+    {
+      throw new NotImplementedException();
     }
 
     protected void TextBox1_TextChanged(object sender, EventArgs e)
@@ -38,19 +39,14 @@ namespace Contrato
       }
       else
       {
+        SqlCommand aceptarContrato = new SqlCommand(@"exec spn_contrato_firma 98531705, 1, Administrador, 67100");
         Response.Redirect("redirect.aspx");
       }
     }
 
     protected void btnimprimir_Click(object sender, EventArgs e)
     {
-      // Render any HTML fragment or document to HTML
-      var Renderer = new IronPdf.HtmlToPdf();
-      var PDF = Renderer.RenderHtmlAsPdf("<h1>Hello IronPdf</h1>");
-      var OutputPath = "HtmlToPDF.pdf";
-      PDF.SaveAs(OutputPath);
-      // This neat trick opens our PDF file so we can see the result in our default PDF viewer
-      System.Diagnostics.Process.Start(OutputPath);
+      
     }
   }
 }
